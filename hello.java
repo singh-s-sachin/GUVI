6801ac7091ff6c5d3781449c05237873b010eecb
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 19:20:17 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class hello
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int k = sc.nextInt();
+		for(int i=0; i<k; i++)
+			System.out.println("Hello");
+}
+}
