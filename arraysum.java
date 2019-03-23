cbb5091ecc0cf4126d52ad2b7eb302ddefcf4c3b
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 19:36:39 +0530

    Add files via upload

---
+++ arraysum.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class arraysum
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int s=0;
+		int k = sc.nextInt();
+		int l= sc.nextInt();
+		int n[]=new int[k];
+		for(int i=0; i<k; i++)
+		{
+			n[i]= sc.nextInt();
+		}
+		for(int i=0; i<l; i++)
+			s+=n[i];
+			System.out.println(s);
+}
+}
