3d5d5349fa633a602ed53b6180a577a70e6c11bc
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 19:11:48 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class leap
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int k = sc.nextInt();
+		if(k%4==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+}
+}
