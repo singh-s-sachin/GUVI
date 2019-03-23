32b564aa3a095caee749b042b4de6960ad0207b1
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 18:40:15 +0530

    Add files via upload

---
+++ checkevenodd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class checkevenodd
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int k = sc.nextInt();
+		if(k%2==0)
+			System.out.println("Even");
+		else 
+			System.out.println("Odd");
+	}
+}
