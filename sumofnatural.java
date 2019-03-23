914c47f6d6bb894cbbe6e4c07ed8e72c2ce7d0c6
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 19:25:05 +0530

    Add files via upload

---
+++ sumofnatural.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sumofnatural
+{
+	public static void main(String args[])
+	{
+		Scanner sc= new Scanner(System.in);
+		int s=0;
+		int k = sc.nextInt();
+		for(int i=1; i<=k; i++)
+			s=s+i;
+			System.out.println(s);
+}
+}
