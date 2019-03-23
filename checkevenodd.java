21290bf8b54dafcdc5c64fcd781d59273c6bb0ce
singh-s-sachin <singh.s.kshetriya@gmail.com>
2019-03-23 18:42:40 +0530

    Update checkevenodd.java

--- checkevenodd.java
+++ checkevenodd.java
@@ -5,9 +5,14 @@ class checkevenodd
 	{
 		Scanner sc= new Scanner(System.in);
 		int k = sc.nextInt();
+		if(k>0=)
+		{
 		if(k%2==0)
 			System.out.println("Even");
 		else 
 			System.out.println("Odd");
+		}
+		else
+			System.out.println("Invalid");
 	}
 }
